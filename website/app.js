b1c86b6581e8efcd60697f0ab57c1beef414e8f1
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-01-20 09:24:14 +0200

    Styling
    Styling

--- website/app.js
+++ website/app.js
@@ -1,10 +1,10 @@
 // Personal API Key for OpenWeatherMap API
 let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
-let apiKey = '&appid=8553a2bea1cdd4c0a872125b795bd7de';
+let apiKey = '&appid=8553a2bea1cdd4c0a872125b795bd7de&mode=json&units=metric';
 
 // Create a new date instance dynamically with JS
 let date = new Date();
-let newDate = date.getMonth() + '.' + date.getDate() + '.' + date.getFullYear()
+let newDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', performAction);
@@ -12,17 +12,19 @@ document.getElementById('generate').addEventListener('click', performAction);
 /* Function called by event listener */
 async function performAction() {
     const zipCode = document.getElementById('zip').value;
-    if (document.getElementById('zip').value.length === 0){
+    if (document.getElementById('zip').value.length === 0) {
         alert("You must enter a zip code");
-    }else {
+    } else {
         const feelings = document.getElementById('feelings').value;
         getWeather(baseURL, zipCode, apiKey)
             .then(await function (data) {
                 // Add data
                 console.log('posting data', data);
-                postWeather('/addWeather', {temp: data.main.temp, date: newDate, userResponse: feelings, cityName: data.name});
+                postWeather('/addWeather', {
+                    temp: data.main.temp, date: newDate, userResponse: feelings, cityName: data.name
+                });
             })
-            .then(function (){
+            .then(function () {
                 updateUI();
             });
     }
@@ -43,12 +45,9 @@ const getWeather = async (baseURL, zipCode, key) => {
 /* Function to POST data */
 const postWeather = async (url = '', data = {}) => {
     const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
+        method: 'POST', credentials: 'same-origin', headers: {
             'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
+        }, body: JSON.stringify(data), // body data type must match "Content-Type" header
     });
 
     try {
@@ -65,10 +64,10 @@ const updateUI = async () => {
     try {
         const allData = await request.json();
         console.log('getting', allData);
-        document.getElementById('city').innerHTML = allData.cityName;
-        document.getElementById('date').innerHTML = allData.date;
-        document.getElementById('temp').innerHTML = allData.temp;
-        document.getElementById('content').innerHTML = allData.userResponse;
+        document.getElementById('city').innerHTML = 'City: ' + allData.cityName;
+        document.getElementById('date').innerHTML = 'Date: ' + allData.date;
+        document.getElementById('temp').innerHTML = 'temperature: ' + allData.temp + '°C';
+        document.getElementById('content').innerHTML = 'Response: ' + allData.userResponse;
 
     } catch (error) {
         console.log("error", error);
